dae887c184f9ec0599da8984a9a03fd2b612d5b5
220-AkifumiTomiyama <akifumi_tomiyama@tbn.co.jp>
2018-02-26 15:36:10 +0900

    test init test

--- lib/generators/swagger/init/init_generator.rb
+++ lib/generators/swagger/init/init_generator.rb
@@ -1,14 +1,14 @@
 module Swagger
   class InitGenerator < ::Rails::Generators::NamedBase
 
-    source_root.File.expand_path('../templates', __FILE__)
+    source_root File.expand_path('../templates', __FILE__)
 
     def create_concern_dir
-      directory nil, concern_dir
+      empty_directory concern_dir
     end
 
     def create_namespace_dir
-      directory nil, namespace_dir
+      empty_directory namespace_dir
     end
 
     def create_swagger_controller_file
