63f1bec34ac6662d17ecd594a231f28719062cad
220-AkifumiTomiyama <akifumi_tomiyama@tbn.co.jp>
2018-02-27 17:28:42 +0900

    feat: namespaced route

--- lib/generators/swagger/init/init_generator.rb
+++ lib/generators/swagger/init/init_generator.rb
@@ -1,3 +1,5 @@
+require 'rails/generators/rails/resource_route/resource_route_generator'
+
 module Swagger
   class InitGenerator < ::Rails::Generators::NamedBase
 
@@ -15,6 +17,12 @@ module Swagger
       template "_swagger_controller.rb.tt", swagger_controller_path
     end
 
+    def insert_route
+      # generate 'resource_route', name_path, verbose: false
+      in_root { run_ruby_script("bundle exec rails generate resource_route #{name_path}", verbose: false) }
+      byebug
+    end
+
     private
     def swagger_controller_path
       File.join("app/controllers", name, "swagger_controller.rb")
@@ -26,6 +34,10 @@ module Swagger
 
     def namespace_dir
       File.join(concern_dir, name)
+    end
+
+    def name_path
+      File.join("/", name, "swagger").sub(/\A\/+/, '')
     end
   end
 end
