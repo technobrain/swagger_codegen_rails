a977df6f0f6ac2fe16c7fa14721429bc132182bb
220-AkifumiTomiyama <akifumi_tomiyama@tbn.co.jp>
2018-03-01 14:05:17 +0900

    require update

--- lib/swagger_codegen_rails.rb
+++ lib/swagger_codegen_rails.rb
@@ -3,6 +3,9 @@ require 'swagger_codegen_rails/configuration'
 require 'swagger_codegen_rails/defaults'
 require 'swagger_codegen_rails/parameter'
 require 'swagger_codegen_rails/parser'
+require 'swagger_codegen_rails/base'
+require 'swagger_codegen_rails/namespace'
+require 'swagger_codegen_rails/konstants'
 
 require 'generators/swagger'
 
